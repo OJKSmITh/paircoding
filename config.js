7bb7bc2eab729cda541074b89007cacf906242bd
mood <chamesh@naver.com>
2023-01-13 11:30:34 +0900

    fix : 기본 설정 살짝 수정

--- config.js
+++ config.js
@@ -1,24 +1,24 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const host = process.env.DB_HOST
-const port = process.env.DB_PORT
-const user = process.env.DB_USER
-const password = process.env.DB_PASSWORD
-const database = process.env.DB_DATABASE
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_DATABASE;
 
 const config = {
     db: {
-        username: "root",
-        password: "95211010!",
-        host: "127.0.0.1",
-        port: '3306',
-        database: 'sample',
-        dialect: "mysql", // 어떤 dbms
-        define: {// 디폴트값을 넣을 수 있음
-            freezeTableName: true, //복수형태 x
+        username,
+        password,
+        host,
+        port,
+        database,
+        dialect: "mysql",
+        define: {
+            freezeTableName: true,
             timestamps: false,
-        }
+        },
     },
-}
+};
 
-module.exports = config
+module.exports = config;
